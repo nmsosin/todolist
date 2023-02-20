2eabd9dd0ac52ba23a2d28107466cd26e3c59038
Nikolay Sosin <102407703+nmsosin@users.noreply.github.com>
2023-02-20 14:19:50 +0300

    Initital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { v1 } from 'uuid';
 import './App.css';
+import TodoList from "./TodoList";
+import {TaskType} from "./TodoList";
 
+export type FilterValuesType = "all" | "completed" | "active";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let initialTasks = [
+        {id: v1(), title: "CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "React", isDone: false},
+        {id: v1(), title: "Redux", isDone: false}
+    ]
+    console.log(initialTasks)
+
+    let [tasks, setTasks] = useState<Array<TaskType>>(initialTasks);
+    let [filter, setFilter] =useState<FilterValuesType>('all');
+
+    function changeFilter (value: FilterValuesType) {
+        setFilter(value);
+    }
+
+    let activeTasks = tasks;
+    if (filter === 'completed') {
+        activeTasks = tasks.filter(t => t.isDone === true)
+    }
+    if (filter === 'active') {
+        activeTasks = tasks.filter(t => t.isDone === false)
+    }
+
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks);
+    }
+
+    return (
+        <div className="App">
+            <TodoList title="What to learn"
+                      tasks={activeTasks}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+            />
+        </div>
+    );
 }
 
 export default App;
